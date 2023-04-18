d01683c9d18a9a083dc55f1581b67694781c5a19
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-04-18 16:09:23 +0430

    signly linked list problem js

---
+++ #234_Palindrome Linked List_js/index.js
@@ -0,0 +1,35 @@
+// Leetcode problem algorithm signly linked list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let slow = head;
+    let fast = head;
+    let stack = [];
+
+    while (fast && fast.next){
+        stack.push(slow.val);
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    if(fast){
+        slow = slow.next;
+    }
+    while(slow){
+        if(stack.pop() !== slow.val){
+            return false;
+        }
+        slow = slow.next;
+    }
+    return true;
+};
